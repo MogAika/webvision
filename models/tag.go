5b5c41700b494273cb498f3f7a838b82dd352d67
Vladimir <mogaika@yandex.ru>
2016-04-06 13:49:37 +0300

    config files support

--- models/tag.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Tag struct {
-	ID   uint64
-	Name string
-}
--- webvision.go
+++ webvision.go
@@ -1,30 +1,41 @@
 package main
 
 import (
+	"flag"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+
 	"github.com/mogaika/webvision/app"
 	"github.com/mogaika/webvision/log"
 	"github.com/mogaika/webvision/settings"
 )
 
-func main() {
-	s := &settings.Settings{
-		DataPath:    "./data/",
-		FFmpeg:      "../ffmpeg-20160404-git-54c9146-win64-static/bin/ffmpeg.exe",
-		MaxDataSize: 1024 * 1024 * 128, // 128 Mbytes
-		DB: settings.DBSettings{
-			Dialect: "sqlite3",
-			Params:  "_test.db",
-		},
-		Web: settings.WebSettings{
-			Host:        "",
-			TlsHost:     "127.0.0.1:4443",
-			TlsCertFile: "server.pem",
-			TlsKeyFile:  "server.key",
-		},
-	}
+var Config *string
 
-	_, err := app.NewApp(s)
+func init() {
+	Config = flag.String("c", "webvision.yaml", "Config file")
+}
+
+func main() {
+	flag.Parse()
+
+	confdata, err := ioutil.ReadFile(*Config)
 	if err != nil {
-		log.Log.Errorf("Application error: %v\n", err)
+		log.Log.Criticalf("Cannot open config file '%s': %v", *Config, err)
+	} else {
+		s := &settings.Settings{}
+		err = yaml.Unmarshal(confdata, s)
+
+		if err != nil {
+			log.Log.Criticalf("Error cannot read yaml config file '%s': %v", *Config, err)
+			os.Exit(1)
+		} else {
+			_, err = app.NewApp(s)
+			if err != nil {
+				log.Log.Errorf("Application error: %v\n", err)
+			}
+		}
 	}
 }
