f6cf34032162b74598a0edb51ef8b75d55715820
Vladimir Jigulin <mogaika@yandex.ru>
2016-04-08 14:44:25 +0300

    added missed file

---
+++ handlers/status.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ricochet2200/go-disk-usage/du"
+)
+
+func HandlerStatus(w http.ResponseWriter, r *http.Request) {
+	_, set := VarsFromRequest(r)
+
+	usage := du.NewDiskUsage(set.DataPath)
+
+	fmt.Fprintf(w, `Storage use %vM/%vM (%v%%)`,
+		usage.Used()/1024/1024, usage.Size()/1024/1024, usage.Usage())
+}
