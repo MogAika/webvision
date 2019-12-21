a0aa2b390613dcce95f1b0994ed75662d0a67750
Vladimir Jigulin <vjigulin@mirantis.com>
2019-12-22 02:30:31 +0400

    fix some bugs in random page

--- static/random.js
+++ static/random.js
@@ -1,47 +1,57 @@
-function wsBlockShow($bl) {
-	$bl.show();
-	var video = $bl.find("video").first();
-	if (video.length) {
-		video[0].play();
+function wsRandomPage(data) {
+	this.cachedBlocks = [];
+	this.currentBlock = null;
+	this.urlPrefix = '/data/';
+	this.savedVolume = 0.5;
+	this.cacheSize = 2;
+}
+
+wsRandomPage.prototype.preload = function() {
+	for (let i = this.cachedBlocks.length; i < this.cacheSize; i++) {
+		this.requestBlock();
 	}
 }
 
-function wsNextVideoClick() {
-	var prevVideo = $(".ws-random-card").first();
-	var volume = prevVideo.find("video")[0].volume;
-	prevVideo.remove();
-	var nextVideo = $(".ws-random-card").first();
-	if (nextVideo.length) {
-		wsBlockShow(nextVideo);
-	}
-	wsRequestRandomBlock();
-	nextVideo.find("video")[0].volume = volume;
-}
+wsRandomPage.prototype.onNext = function() {
+	if (this.currentBlock) {
+		let plel = this.currentBlock.getPlayableElement();
+		if (plel) {
+			this.savedVolume = plel[0].volume;
+		}
 
-function wsCreateRandomBlock(o) {
-	var ptype = o.Type.split('/')[0];
-	switch (ptype) {
-		case "audio":
-			var card = '<audio controls preload="none" loop><source src="' + o.Url + '"></audio>';
-			break;
-		case "video":
-			var card = '<video controls loop><source src="' + o.Url + '"></source></div>';
-			break;
-		case "image":
-			var card = '<a style="background-image:url(\'' + o.Url + '\'" href="' + o.Url + '" onclick="return false;">';
-			break;
+		this.currentBlock.div.remove();
+		this.currentBlock = null;
 	}
-	var block = $('<div class="ws-random-card"><div class="ws-data ws-data-' + ptype + '">' + card + '</div></div>').hide();
+
+	this.currentBlock = this.cachedBlocks.shift();	
+	if (this.currentBlock) {
+		let plel = this.currentBlock.getPlayableElement();
+		if (plel) {
+			plel[0].volume = this.savedVolume;
+			plel[0].play();
+		}
+
+		this.currentBlock.div.show();
+	}
 	
-	$(".container").append(block);
-	if ($(".ws-random-card").length === 1) {
-		wsBlockShow(block);
-	}
-
-	return block;
+	this.preload();
 }
 
-function wsRequestRandomBlock() {
+wsRandomPage.prototype.onBlockReceived = function(data) {
+	data.Url = this.urlPrefix + data.Url;
+	let block = new wsRandomBlock(data);
+	this.cachedBlocks.push(block);
+	
+	let elblock = block.createElement();
+	$(".container").append(elblock);
+	
+	if (!this.currentBlock) {
+		this.onNext();
+	}
+}
+
+wsRandomPage.prototype.requestBlock = function() {
+	let randomPage = this;
 	$.ajax({
 		url: "/api/random",
 		method: "get",
@@ -50,14 +60,63 @@ function wsRequestRandomBlock() {
 			console.log("fail", result);
 		},
 		success: function(data) {
-			data.Url = "/data/" + data.Url;
-			wsCreateRandomBlock(data);
+			randomPage.onBlockReceived(data);
 		}
 	});
 }
 
+
+function wsRandomBlock(data) {
+    this.data = data;
+    this.div = undefined;
+}
+
+wsRandomBlock.prototype.getMediaType = function() {
+    return this.data.Type.split('/')[0];
+}
+
+wsRandomBlock.prototype.getElement = function() {
+	return this.div;
+}
+
+wsRandomBlock.prototype.createElement = function() {
+	let type = this.getMediaType();
+	let card;
+	let o = this.data;
+	switch (type) {
+		case "audio":
+			card = '<audio controls preload="none" loop><source src="' + o.Url + '"></audio>';
+			break;
+		case "video":
+			card = '<video controls loop><source src="' + o.Url + '"></source></div>';
+			break;
+		case "image":
+			card = '<a style="background-image:url(\'' + o.Url + '\'" href="' + o.Url + '" onclick="return false;">';
+			break;
+		default:
+			card = '<h2>Unknown card type ' + type + '</h2>';
+			break;
+	}
+	this.div = $('<div class="ws-random-card"><div class="ws-data ws-data-' + type + '">' + card + '</div></div>').hide();
+	return this.div;
+}
+
+wsRandomBlock.prototype.getPlayableElement = function() {
+	switch (this.getMediaType()) {
+		case "video":
+			return this.div.find("video");
+		case "audio":
+			return this.div.find("audio");
+		default:
+			return null;
+	}
+}
+
 window.onload = function() {
-	wsRequestRandomBlock();
-	wsRequestRandomBlock();
-	$("#ws-next-video-btn").click(wsNextVideoClick);
+	let randomPage = new wsRandomPage();
+	$("#ws-next-video-btn").click(function() {
+		randomPage.onNext();
+	});
+	
+	randomPage.preload();
 };
